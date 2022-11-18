ddf0fee1d7cebd8e3cbcac9c26e9ff1fba00ceb4
Kuba Ciukiewicz <jc2195@cam.ac.uk>
2022-11-18 01:56:59 +0000

    Close camera after picture.
    Signed-off-by: Kuba Ciukiewicz <jc2195@cam.ac.uk>

--- src/helpers/cv.py
+++ src/helpers/cv.py
@@ -1,22 +1,6 @@
 import cv2
 import numpy as np
 
-def is_part_there(img):
-
-    """
-    Fist checks if an image is grey scaled and changes it if not
-    This looks at the middle pixel of an image,
-    assesses whether it is a part or the black background
-    """
-    if len(np.array(img).shape) != 2:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    half = len(img)//2
-    if img[half, half] > 100:
-        return True
-    else:
-        return False
-
 class ImageManipulation:
     def trimImage(image, boundary):
         image = image[
@@ -31,6 +15,16 @@ class ImageManipulation:
 
     def morphologyOpen(image):
         return cv2.morphologyEx(image, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9)))
+
+    def presenceDetection(image):
+        if len(np.array(image).shape) != 2:
+            image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        half = len(image)//2
+        if image[half, half] > 100:
+            return True
+        else:
+            return False
 
 class Contours:
     def getContourByArea(image, minArea = -float('Inf'), maxArea = float('Inf')):
