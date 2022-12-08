2261753485a3831711cece56ab804c82101b1e51
Kuba Ciukiewicz <jc2195@cam.ac.uk>
2022-12-08 12:25:01 +0000

    Modify replenish procedure. Scale down resolution of display image. Limit number of replenished parts of each type to 2.
    Signed-off-by: Kuba Ciukiewicz <jc2195@cam.ac.uk>

--- src/helpers/display.py
+++ src/helpers/display.py
@@ -11,6 +11,13 @@ class Viewer:
                     im.seek(-2,2)
                     if im.read() == b'\xff\xd9':
                         image = cv2.imread(f"images/live/99.jpg")
+                        height = image.shape[0]
+                        width = image.shape[1]
+                        multiplier = 720 / width
+                        image = cv2.resize(image, (int(width * multiplier), int(height * multiplier)))
                         cv2.imshow("Latest inspection", image)
                         cv2.waitKey(1)
                         now = time.time()
+
+viewer = Viewer()
+viewer.show()
